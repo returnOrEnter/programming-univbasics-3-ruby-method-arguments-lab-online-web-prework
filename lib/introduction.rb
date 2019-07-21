c53d30dc059c0c592a77c1c93713f715699289a3
Ivan Flores <triskelion1122@gmail.com>
2019-07-21 19:57:42 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,8 +13,8 @@ end
 introduction_with_language("Dan", "Ember.js")
 introduction_with_language("Josh", "React")
 
-def introduction_with_language_optional(name, language)
+def introduction_with_language_optional(name, language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language_optional("")
+introduction_with_language_optional("Edwin")
