29de5971922fa5c6a53b6054fce627e21aa7a72c
Ivan Flores <triskelion1122@gmail.com>
2019-07-21 19:46:47 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,5 +9,5 @@ introduction("Dan")
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
   
-introduction_with_language("Josh", "Ember.js")
-introduction_with_language("Dan", "React")
+introduction_with_language("Dan", "Ember.js")
+introduction_with_language("Josh", "React")
