7e2483dcd39ee942ad1716012fe50b4b880026ea
Ivan Flores <triskelion1122@gmail.com>
2019-07-21 19:45:51 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,13 @@
+
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+  
+introduction("Josh")
+introduction("Dan")
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  
+introduction_with_language("Josh", "Ember.js")
+introduction_with_language("Dan", "React")
